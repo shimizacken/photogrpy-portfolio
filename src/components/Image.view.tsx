e20f7ad5f5197aa73755fe5b19af5915f6c69a28
Shimi Zacken <shimi.zacken@sesam.io>
2025-06-23 09:15:03 +0200

    styles

--- src/components/Image.view.tsx
+++ src/components/Image.view.tsx
@@ -3,20 +3,8 @@ import { Photo } from "../types/photos.types";
 
 export const Image: React.FC<{ photo: Photo }> = ({ photo }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "60%",
-      }}
-    >
-      <img
-        src={photo.path}
-        width="100%"
-        style={{ border: "5vw solid #fff", boxSizing: "border-box" }}
-      />
-      <div>{}</div>
+    <div className="image-container">
+      <img src={photo.path} width="100%" className="image" />
     </div>
   );
 };
